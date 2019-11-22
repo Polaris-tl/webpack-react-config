107f0078f2c2d7e8f2c162c5f97c4ac7ebe966c2
Polaris_tl <polaris66tl@gmail.com>
2019-11-22 11:35:17 +0800

    webpack-react-config

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './css/style.css';   // 引入css
+import './css/style.less'; // 引入less
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+)
+
+if (module.hot) {
+  // 实现热更新
+  module.hot.accept();
+}
+
